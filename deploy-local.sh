f12e9e3cba0a06baca1913de25b31422e918fe9c
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2024-05-01 11:10:00 +0100

    env file switching

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,8 +1,11 @@
 set -e
+cp .env-local .env
+cp .env-local contracts/.env
+
 source .env
 
 cd downstream
-ds -z $DS_ZONE -k $DS_DEPLOY_KEY -n local apply -R -f .
+ds -z $DS_ZONE -k $DS_DEPLOY_KEY -n $DS_NETWORK apply -R -f .
 
 cd ../contracts
-forge script --rpc-url http://localhost:8545 --broadcast -vvv script/InitTurfWars.s.sol
+forge script --rpc-url $RPC_LOCAL --broadcast -vvv script/InitTurfWars.s.sol
